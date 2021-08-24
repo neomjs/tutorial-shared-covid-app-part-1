f47b58f17b8eef743920a91f671c121f4a02e18c
tobiu <tobiasuhlig78@gmail.com>
2021-08-24 14:04:53 +0200

    empty viewport

--- apps/covid/view/MainContainer.mjs
+++ apps/covid/view/MainContainer.mjs
@@ -1,6 +1,4 @@
-import Component    from '../../../node_modules/neo.mjs/src/component/Base.mjs';
-import TabContainer from '../../../node_modules/neo.mjs/src/tab/Container.mjs';
-import Viewport     from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
+import Viewport from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
 
 /**
  * @class Covid.view.MainContainer
@@ -9,35 +7,8 @@ import Viewport     from '../../../node_modules/neo.mjs/src/container/Viewport.m
 class MainContainer extends Viewport {
     static getConfig() {return {
         className: 'Covid.view.MainContainer',
-        autoMount: true,
-        layout   : {ntype: 'fit'},
-
-        items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
-        }]
+        items    : [],
+        layout   : {ntype: 'fit'}
     }}
 }
 
